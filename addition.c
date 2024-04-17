9614d9dc380c40b79c1c31be97376f7e3635429a
Kiran Bhatt <165576401+kiranbhatt0106@users.noreply.github.com>
2024-04-17 19:31:29 +0545

    Add files via upload
    This is how can we add two numbers.

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int A, B, sum;
+    printf(" enter a number A \n");
+    scanf("%d", &A);
+    printf("enter a number B \n");
+    scanf("%d", &B);
+    sum = (A+B);
+    printf("the value of sum is=%d", sum);
+    return 0;
+}
